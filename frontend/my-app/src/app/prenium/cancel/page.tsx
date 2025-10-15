c98c035a217d6ed0399231e7a9d0cd02c19dfecd
shirley valeton <shirley.valeton88@gmail.com>
2025-10-15 12:37:57 +0200

    commit 12:37 15/10/25

---
+++ frontend/my-app/src/app/prenium/cancel/page.tsx
@@ -0,0 +1,11 @@
+export default function CancelPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Paiement annulé</h1>
+      <p>Vous pouvez réessayer quand vous voulez.</p>
+      <p style={{ marginTop: 12 }}>
+        <a href="/pricing">Retour à la page tarifs</a>
+      </p>
+    </main>
+  );
+}
