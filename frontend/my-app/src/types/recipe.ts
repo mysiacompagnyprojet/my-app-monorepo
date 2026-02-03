9700983e89d4b1e624da75b3113fd1ffc18e9961
Shirley <shirley@MacBookPro.lan>
2026-02-03 18:08:53 +0100

    commit 03/02 18:08

--- frontend/my-app/src/types/recipe.ts
+++ frontend/my-app/src/types/recipe.ts
@@ -25,3 +25,23 @@ trash?: string[];
 totalCostEur?: number | null;
 };
 
+export type RecipeIngredient = {
+    id? : string
+    name: string
+    quantity: number
+    unit: string
+    costRecipe?: number | null
+    buyPriceEur?: number | null
+    airtableId?: string | null
+}
+
+export type Recipe = {
+    id: string
+    title: string
+    servings: number
+    imageUrl: string | null
+    createdAt: string
+    notes?: string
+    steps?: string [] | null
+    ingredients?: RecipeIngredient[]
+}
