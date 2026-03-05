88c3fc1147a6495454bf0e4bfb259075c98fdea2
Shirley <shirley@MacBookPro.lan>
2026-03-05 09:56:43 +0100

    commit 05/03 9h56

--- frontend/my-app/src/app/layout.tsx
+++ frontend/my-app/src/app/layout.tsx
@@ -26,6 +26,11 @@ title: 'MySia-app',
 description: 'Application de recettes et budget',
 }
 
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 return (
 <html lang="fr">
